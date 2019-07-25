66d7bf6076ea86a4355dc8f00294be65cc0a0b1d
Romain F. T <rrroschan@gmail.com>
2019-07-25 22:09:21 +0200

    New version

--- deb_package.sh
+++ deb_package.sh
@@ -8,18 +8,21 @@ VERSION="0.4"
 previous_dir=`pwd`
 
 # mettre en place la structure à la con voulue par les scripts de debian
-DIR_NAME=$PACKAGE_NAME'_'$VERSION
-mkdir -p /tmp/building-dir/$DIR_NAME
-cp -r build-aux /tmp/building-dir/$DIR_NAME
-cp -r data /tmp/building-dir/$DIR_NAME
-cp -r debian /tmp/building-dir/$DIR_NAME
-cp -r help /tmp/building-dir/$DIR_NAME
-cp -r po /tmp/building-dir/$DIR_NAME
-cp -r src /tmp/building-dir/$DIR_NAME
-cp meson.build /tmp/building-dir/$DIR_NAME
-cd /tmp/building-dir/
-tar -Jcvf $PACKAGE_NAME.orig.tar.xz $DIR_NAME
-cd /tmp/building-dir/$DIR_NAME
+DIR_NAME=$PACKAGE_NAME'-'$VERSION
+FILE_NAME=$PACKAGE_NAME'_'$VERSION
+DIR_PATH=/tmp/building-dir
+mkdir -p $DIR_PATH/$DIR_NAME/
+cp -r build-aux $DIR_PATH/$DIR_NAME/
+cp -r data $DIR_PATH/$DIR_NAME/
+cp -r debian $DIR_PATH/$DIR_NAME/
+cp -r help $DIR_PATH/$DIR_NAME/
+cp -r po $DIR_PATH/$DIR_NAME/
+cp -r src $DIR_PATH/$DIR_NAME/
+cp meson.build $DIR_PATH/$DIR_NAME/
+cd $DIR_PATH/
+tar -Jcvf $FILE_NAME.orig.tar.xz $DIR_NAME
+# tar -Jcvf $PACKAGE_NAME.orig.tar.xz $DIR_NAME
+cd $DIR_PATH/$DIR_NAME/
 
 # création des fichiers exigés
 ## création de 'control' à partir des appdata
@@ -29,18 +32,20 @@ cd /tmp/building-dir/$DIR_NAME
 # TODO
 
 ## création de 'compat'
-echo '10' > /tmp/building-dir/$DIR_NAME/debian/compat
+echo '10' > $DIR_PATH/$DIR_NAME/debian/compat
 
 # le truc qui chie des fichiers supplémentaires pour la construction
-dh_make -i
+dh_make -i -y
 
 # la construction
 debuild -i -us -uc -b
 
 # récupérer le paquet dans /tmp
-cp /tmp/building-dir/drawing_0.4_amd64.deb $previous_dir
+cp $DIR_PATH/*.deb $previous_dir
 
 # nettoyage
-# rm -r /tmp/building-dir
+ls $DIR_PATH
+ls $DIR_PATH/$DIR_NAME
+rm -r $DIR_PATH
 cd $previous_dir
 
