24e68ce222e8e7b90c66026594de26b0205d1ee3
Romain F. T <rrroschan@gmail.com>
2019-08-19 18:04:59 +0200

    Minor modifications of the debian script
    No more debuild needed for building the package

--- deb_package.sh
+++ deb_package.sh
@@ -2,9 +2,16 @@
 
 DISTRO="unstable" # TODO lister les valeurs possibles (debian ? ubuntu ? elementary ?)
 PACKAGE_NAME="drawing" # TODO et pour elementary ?
-VERSION="0.4"
+VERSION="0.4.3"
 
-# se souvenir du dossier courant (qui est la racine du projet)
+echo "targeted distribution: $DISTRO"
+echo "package name: $PACKAGE_NAME"
+echo "package version: $VERSION"
+echo ""
+echo "Is it correct? [Return/^C]" # XXX ptêt écrasé dans la suite des opérations ?
+read confirmation
+
+# se souvenir du dossier courant (qui est la racine du projet) pour y amener le paquet à la fin
 previous_dir=`pwd`
 
 # mettre en place la structure à la con voulue par les scripts de debian
@@ -21,24 +28,13 @@ cp -r src $DIR_PATH/$DIR_NAME/
 cp meson.build $DIR_PATH/$DIR_NAME/
 cd $DIR_PATH/
 tar -Jcvf $FILE_NAME.orig.tar.xz $DIR_NAME
-# tar -Jcvf $PACKAGE_NAME.orig.tar.xz $DIR_NAME
 cd $DIR_PATH/$DIR_NAME/
 
-# création des fichiers exigés
-## création de 'control' à partir des appdata
-# TODO
-
-## création de 'changelog' à partir des appdata
-# TODO
-
-## création de 'compat'
-echo '10' > $DIR_PATH/$DIR_NAME/debian/compat
-
-# le truc qui chie des fichiers supplémentaires pour la construction
+# création automatique des fichiers supplémentaires pour la construction
 dh_make -i -y
 
 # la construction
-debuild -i -us -uc -b
+dpkg-buildpackage -i -us -uc -b
 
 # récupérer le paquet dans /tmp
 cp $DIR_PATH/*.deb $previous_dir
