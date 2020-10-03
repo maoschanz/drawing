c00786b5da51f5db327d0863c93aa30b5f58dbfd
Sebastian Krzyszkowiak <dos@dosowisko.net>
2020-10-04 01:46:49 +0200

    Pass the explicit length argument to Pango.Layout's set_text in text tool
    This is required by older binding versions.
    
    Fixes #275

--- src/tools/classic_tools/tool_text.py
+++ src/tools/classic_tools/tool_text.py
@@ -261,7 +261,7 @@ class ToolText(AbstractClassicTool):
 
 	def _show_text_with_options(self, cc, pl, text, text_x, text_y):
 		cc.move_to(text_x, text_y)
-		pl.set_text(text)
+		pl.set_text(text, -1)
 		PangoCairo.update_layout(cc, pl)
 		PangoCairo.show_layout(cc, pl)
 
