d01baf93f9d5aec603dc2164b711e7e4b36ad6d5
Romain F. T <rrroschan@gmail.com>
2021-01-03 00:06:47 +0100

    Merge speed-sensitive and pressure-sensitive dynamic width brushes
    issue #260 #69

--- src/tools/classic_tools/tool_experiment.py
+++ src/tools/classic_tools/tool_experiment.py
@@ -11,7 +11,6 @@ class ToolExperiment(AbstractClassicTool):
 	def __init__(self, window, **kwargs):
 		super().__init__('experiment', _("Experiment"), 'applications-utilities-symbolic', window)
 		self.row.get_style_context().add_class('destructive-action')
-		self._path = None
 
 		# In order to draw pressure-sensitive lines, the path is collected as
 		# an array whose elements are dicts (keys are 'x', 'y', 'p'). An actual
@@ -87,22 +86,18 @@ class ToolExperiment(AbstractClassicTool):
 		self.x_press = event_x
 		self.y_press = event_y
 		self.set_common_values(event.button, event_x, event_y)
-		self._path = None
 		self._manual_path = []
 		self._add_pressured_point(event_x, event_y, event)
 
 	def on_motion_on_area(self, event, surface, event_x, event_y):
-		self._add_point(event_x, event_y)
 		self._add_pressured_point(event_x, event_y, event)
 		operation = self.build_operation()
 		self.do_tool_operation(operation)
 
 	def on_release_on_area(self, event, surface, event_x, event_y):
-		self._add_point(event_x, event_y)
 		self._add_pressured_point(event_x, event_y, event)
 		operation = self.build_operation()
 		operation['is_preview'] = False
-		# TODO dans un monde parfait c'est là qu'on smooth le path je pense
 		self.apply_operation(operation)
 
 	############################################################################
@@ -141,15 +136,6 @@ class ToolExperiment(AbstractClassicTool):
 			return None
 		return pressure
 
-	def _add_point(self, event_x, event_y):
-		cairo_context = self.get_context()
-		if self._path is None:
-			cairo_context.move_to(self.x_press, self.y_press)
-		else:
-			cairo_context.append_path(self._path)
-		cairo_context.line_to(event_x, event_y)
-		self._path = cairo_context.copy_path()
-
 	############################################################################
 
 	def build_operation(self):
@@ -163,13 +149,12 @@ class ToolExperiment(AbstractClassicTool):
 			'line_join': cairo.LineJoin.ROUND,
 			'antialias': self._use_antialias,
 			'is_preview': True,
-			'path': self._path,
-			'manual_path': self._manual_path
+			'path': self._manual_path
 		}
 		return operation
 
 	def do_tool_operation(self, operation):
-		if operation['path'] is None:
+		if operation['path'] is None or len(operation['path']) == 0:
 			return
 		cairo_context = self.start_tool_operation(operation)
 		cairo_context.set_line_cap(operation['line_cap'])
@@ -180,13 +165,7 @@ class ToolExperiment(AbstractClassicTool):
 		if operation['is_preview']:
 			self.op_simple(operation, cairo_context)
 			return # Previewing helps performance and debug
-		if operation['mode'] == 'dynamic1':
-			self.op_dynamic1_segments(operation, cairo_context)
-		elif operation['mode'] == 'dynamic2':
-			self.op_dynamic2_superposition(operation, cairo_context)
-		elif operation['mode'] == 'dynamic3':
-			self.op_dynamic3_mask(operation, cairo_context) # TODO
-		elif operation['mode'] == 'pressure':
+		if operation['mode'] == 'pressure':
 			self.op_pressure(operation, cairo_context)
 		elif operation['mode'] == 'smooth':
 			# self.op_simple(operation, cairo_context)
@@ -199,128 +178,68 @@ class ToolExperiment(AbstractClassicTool):
 	def op_simple(self, operation, cairo_context):
 		cairo_context.set_operator(operation['operator'])
 		cairo_context.set_line_width(operation['line_width'])
-		cairo_context.append_path(operation['path'])
+		cairo_context.new_path()
+		for pt in operation['path']:
+			cairo_context.line_to(pt['x'], pt['y'])
 		cairo_context.stroke()
 
 	def op_smooth(self, operation, cairo_context):
 		cairo_context.set_operator(cairo.Operator.OVER)
 		cairo_context.set_line_width(operation['line_width'])
-		utilities_smooth_path(cairo_context, operation['path'])
+
+		# Build a cairo path from the raw data
+		cairo_context.new_path()
+		for pt in operation['path']:
+			cairo_context.line_to(pt['x'], pt['y'])
+		path = cairo_context.copy_path()
+
+		# Smooth it
+		cairo_context.new_path()
+		utilities_smooth_path(cairo_context, path)
+
+		# Draw it
 		cairo_context.stroke()
 
 	############################################################################
 
-	def op_dynamic1_segments(self, operation, cairo_context):
-		"""Brush with speed-sensitive dynamic width, where the variation of
-		width is drawn by a succession of segments. The first segment is shit,
-		and it looks awful with semi-transparent colors."""
-
-		# mask intersections between segments when semi-transparency
-		cairo_context.set_operator(cairo.Operator.SOURCE)
-
-		SMOOTH = True
-		if SMOOTH:
-			utilities_smooth_path(cairo_context, operation['path'])
-			path = cairo_context.copy_path()
-		else:
-			path = operation['path']
-		line_width = 0
-		cairo_context.new_path()
-
-		for pts in path:
-			ok, future_x, future_y = self._future_point(pts)
-			if not ok:
-				continue
-			current_x, current_y = cairo_context.get_current_point()
-			dist = self._get_dist(current_x, current_y, future_x, future_y)
-			new_width = 1 + int( operation['line_width']/max(1, 0.05 * dist) )
-			if line_width == 0:
-				line_width = new_width
+	def _build_widths(self, manual_path, base_width):
+		"""Build an array of widths from the raw data, either using the value of
+		the pressure or based on the estimated speed of the movement."""
+		widths = []
+		dists = []
+		p2 = None
+		for pt in manual_path:
+			if pt['p'] is None:
+				# No data about pressure
+				if p2 is not None:
+					dists.append(self._get_dist(pt['x'], pt['y'], p2['x'], p2['y']))
 			else:
-				line_width = (new_width + line_width) / 2
-			line_width = max(1, line_width)
-			cairo_context.set_line_width(line_width)
-			self._add_segment(cairo_context, pts)
-			cairo_context.stroke()
-			cairo_context.move_to(future_x, future_y)
+				# There are data about pressure
+				if p2 is not None:
+					if p2['p'] == 0 or pt['p'] == 0:
+						seg_width = 0
+					else:
+						seg_width = (p2['p'] + pt['p']) / 2
+					# A segment whose 2 points have a 50% pressure shall have a
+					# width of "100%" of the base_width, so "base * mean * 2"
+					widths.append(base_width * seg_width * 2)
+			p2 = pt
 
-	def op_dynamic2_superposition(self, operation, cairo_context):
-		"""Brush with speed-sensitive dynamic width, where the variation of
-		width is drawn by a superposition of segments. It doesn't really support
-		semi-transparent colors."""
-
-		# cairo_context.set_operator(operation['operator'])
-		SMOOTH = True
-		if SMOOTH:
-			utilities_smooth_path(cairo_context, operation['path'])
-			path = cairo_context.copy_path()
-		else:
-			path = operation['path']
-		base_width = operation['line_width']
-		line_width = 0
-		cairo_context.new_path()
-
-		length = 1
-		for pts in path:
-			length += 1
-		widths = [base_width] * length
-
-		# Initializing widths (XXX faisable sans cairo_context) + factorisable
-		# TODO pas correct, la width a un "retard" sur la vitesse du tracé
-		i = 0
-		for pts in path:
-			i += 1
-			ok, future_x, future_y = self._future_point(pts)
-			if not ok:
-				continue
-			current_x, current_y = cairo_context.get_current_point()
-			dist = self._get_dist(current_x, current_y, future_x, future_y)
-			new_width = 1 + int(base_width / max(1, 0.05 * dist))
-			if line_width == 0:
-				line_width = (new_width + base_width) / 2
-			else:
-				line_width = (new_width + line_width + line_width) / 3
-			widths[i] = max(1, int(line_width))
-			cairo_context.move_to(future_x, future_y)
-
-		# For each possible width, draw some portions of the path
-		line_width = base_width
-		cairo_context.new_path()
-		while line_width > 1:
-			cairo_context.set_line_width(line_width)
-			to_draw = [True] * length
-			for i in range(length):
-				to_draw[i] = widths[i] >= line_width
-			empty = True
-			i = 0
-			for pts in path:
-				i += 1
-				ok, future_x, future_y = self._future_point(pts)
-				if not ok:
-					# print("not ok", i)
-					continue
-				current_x, current_y = cairo_context.get_current_point()
-				if to_draw[i]:
-					empty = False
-					# print("draw", i, "with width", line_width)
-					self._add_segment(cairo_context, pts)
+		# If nothing in widths, it has to be filled from dists
+		if len(widths) == 0:
+			min_dist = min(dists)
+			max_dist = max(dists)
+			temp_width = 0
+			for dist in dists:
+				new_width = 1 + int(base_width / max(1, 0.05 * dist))
+				if temp_width == 0:
+					temp_width = (new_width + base_width) / 2
 				else:
-					if not empty:
-						cairo_context.stroke()
-						empty = True
-				cairo_context.move_to(future_x, future_y)
-			cairo_context.stroke()
-			cairo_context.new_path()
-			line_width = max(line_width - 1, 1)
-		# Would be needed if the while loop was less exhaustive
-		# cairo_context.set_line_width(line_width)
-		# cairo_context.append_path(path)
-		# cairo_context.stroke()
+					temp_width = (new_width + temp_width + temp_width) / 3
+				width = max(1, int(temp_width))
+				widths.append(width)
 
-	def op_dynamic3_mask(self, operation, cairo_context):
-		pass
-		# TODO support operators and semi-transparency, using tricks like what
-		# the dest_in operator is doing to blur. Same superposition as dynamic2
+		return widths
 
 	def _add_segment(self, cairo_context, pts):
 		if pts[0] == cairo.PathDataType.CURVE_TO:
@@ -344,44 +263,22 @@ class ToolExperiment(AbstractClassicTool):
 	############################################################################
 
 	def op_pressure(self, operation, cairo_context):
-		# -> mieux factorisable !
-		"""..."""
+		"""Brush with dynamic width, where the variation of width is drawn by a
+		succession of segments. If pressure is detected, the width is pressure-
+		sensitive, otherwise it's speed-sensitive (with a heavy ponderation to
+		make it less ugly).
+		For now it looks awful with semi-transparent colors."""
 
-		# 1. construction d'un path et initialisation d'un array de width
-		line_width = operation['line_width']
-		widths = []
-		dists = []
+		# Build an array with all the widths for each segment
+		widths = self._build_widths(operation['path'], operation['line_width'])
+
+		# Build a raw path with lines between the points
 		cairo_context.new_path()
-		p2 = None
-		for pt in operation['manual_path']:
+		for pt in operation['path']:
 			cairo_context.line_to(pt['x'], pt['y'])
-			if pt['p'] is None:
-				# No data about pressure
-				if p2 is not None:
-					dists.append(self._get_dist(pt['x'], pt['y'], p2['x'], p2['y']))
-			else:
-				# There are data about pressure
-				if p2 is not None:
-					if p2['p'] == 0 or pt['p'] == 0:
-						seg_width = 0
-					else:
-						seg_width = (p2['p'] + pt['p']) / 2
-					# A segment whose 2 points have a 50% pressure shall have a
-					# width of "100%" of operation['line_width'], so "* 2"
-					widths.append(line_width * seg_width * 2)
-			p2 = pt
-
-		# If nothing in widths, it has to be filled from dists
-		if len(widths) == 0:
-			min_dist = min(dists)
-			max_dist = max(dists)
-			for dist in dists:
-				width = line_width # TODO
-				widths.append(width)
-
 		raw_path = cairo_context.copy_path()
 
-		# 2. smoothing du path
+		# Smooth this raw path
 		cairo_context.new_path()
 		utilities_smooth_path(cairo_context, raw_path)
 		smoothed_path = cairo_context.copy_path()
@@ -389,7 +286,8 @@ class ToolExperiment(AbstractClassicTool):
 		# 3. création d'un contexte d'une surface vierge
 		# TODO
 
-		# 4. parcours du path : dessin manuel (op source) avec widths associées
+		# Run through the path to manually draw each segment with its width
+		cairo_context.set_operator(cairo.Operator.SOURCE)
 		i = 0
 		cairo_context.new_path()
 		for segment in smoothed_path:
@@ -405,6 +303,7 @@ class ToolExperiment(AbstractClassicTool):
 			cairo_context.move_to(future_x, future_y)
 
 		# 5. painting sur la vraie surface selon l'opérateur choisi
+		cairo_context.set_operator(operation['operator'])
 		# TODO
 
 	############################################################################
