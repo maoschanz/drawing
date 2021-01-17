89253f040a4629ca99886fc7018770f24f17ffb4
Romain F. T <rrroschan@gmail.com>
2021-01-17 03:08:17 +0100

    update copyright years to 2021

--- src/tools/classic_tools/tool_highlight.py
+++ src/tools/classic_tools/tool_highlight.py
@@ -1,6 +1,6 @@
 # tool_highlight.py
 #
-# Copyright 2018-2020 Romain F. T.
+# Copyright 2018-2021 Romain F. T.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
