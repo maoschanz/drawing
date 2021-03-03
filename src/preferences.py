d12e1b8f69ec200aa6198c3bb19a12ae89678863
Romain F. T <rrroschan@gmail.com>
2021-03-03 02:32:20 +0100

    make 'brush' an optional (disabled by default) tool, and update the help

--- src/preferences.py
+++ src/preferences.py
@@ -142,6 +142,8 @@ class DrPrefsWindow(Gtk.Window):
 		tools_dict = {
 			'eraser': _("Eraser"),
 			'highlight': _("Highlighter"),
+			# 'censor': _("Censor"),
+			'brush': _("Brush"),
 			'points': _("Points"),
 			'free_select': _("Free selection"),
 			'color_select': _("Color selection"),
