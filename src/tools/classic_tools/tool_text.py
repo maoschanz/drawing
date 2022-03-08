f21e627a8f31fe93821abb0b05d233d14387c0be
Romain F. T <rrroschan@gmail.com>
2022-03-08 02:10:35 +0100

    new attempt to fix #479

--- src/tools/classic_tools/tool_text.py
+++ src/tools/classic_tools/tool_text.py
@@ -54,23 +54,28 @@ class ToolText(AbstractClassicTool):
 	# Options ##################################################################
 
 	def _set_font(self, *args):
+		Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, self._set_font_async, {})
+
+	def _set_font_async(self, *args):
+		"""Incorrect fonts may trigger `Pango.CRITICAL` errors, which are fine
+		but i don't want the entire app to go down when it happens. The solution
+		seems to be a 2nd thread.
+		This asynchrous function is actually on a different thread, it's not
+		just a shitty timeout-based locking system."""
 		dialog = Gtk.FontChooserDialog(show_preview_entry=False)
 		dialog.set_level(Gtk.FontChooserLevel.FAMILY)
 		dialog.set_font(self._font_fam_name)
 
 		# for f in PangoCairo.font_map_get_default().list_families():
 		# 	print(f.get_name())
-		try:
-			status = dialog.run()
-			if(status == Gtk.ResponseType.OK):
-				self._font_fam_name = dialog.get_font_family().get_name()
-				# print(dialog.get_font())
-				font_gvar = GLib.Variant.new_string(self._font_fam_name)
-				self.window.lookup_action('text-active-family').set_state(font_gvar)
-				self._preview_text()
-		except ex as Exception:
-			print(ex) # XXX mmhmh?
-			return
+
+		status = dialog.run()
+		if(status == Gtk.ResponseType.OK):
+			self._font_fam_name = dialog.get_font_family().get_name()
+			# print(dialog.get_font())
+			font_gvar = GLib.Variant.new_string(self._font_fam_name)
+			self.window.lookup_action('text-active-family').set_state(font_gvar)
+			self._preview_text()
 		dialog.destroy()
 
 	def _set_font_options(self, *args):
