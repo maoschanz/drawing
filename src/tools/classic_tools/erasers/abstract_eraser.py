cfee91f9f29a211c9f6e95766f05324aaadb027b
RomainT <rrroschan@gmail.com>
2022-04-04 12:17:58 +0200

    empty files for the new classes of #503

---
+++ src/tools/classic_tools/erasers/abstract_eraser.py
@@ -0,0 +1,7 @@
+# Licensed under GPL3 https://github.com/maoschanz/drawing/blob/master/LICENSE
+
+import cairo
+
+class AbstractEraser():
+	__gtype_name__ = 'AbstractEraser'
+
---
+++ src/tools/classic_tools/erasers/eraser_area.py
@@ -0,0 +1,8 @@
+# Licensed under GPL3 https://github.com/maoschanz/drawing/blob/master/LICENSE
+
+import cairo
+from .abstract_eraser import AbstractEraser
+
+class EraserArea(AbstractEraser):
+	__gtype_name__ = 'EraserArea'
+
